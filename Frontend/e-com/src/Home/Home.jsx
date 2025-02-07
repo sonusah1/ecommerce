b89f95ff5ea044b4fa808e14ac4d29bcb1545dab
sonusah1 <roshansah2267@gmail.com>
2025-02-07 13:07:48 +0530

    Added new files

---
+++ Frontend/e-com/src/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Nav from '../Users/Nav'
+import Carousel from '../Users/Carousel '
+import Grid from '../Users/Grid';
+import Footer from './Footer';
+import MainNav from '../Users/MainNav';
+import SliderComponent from './SliderComponent';
+import Product from './Product';
+import MetaData from './MetaData';
+import Products from '../Users/Products';
+
+
+const Home = () => {
+  return (
+    <div>
+      <MetaData title={"Shop Smart"} />
+        <MainNav/>
+ 
+
+        <SliderComponent/>
+        <Product/>
+        <Products/>
+        
+      
+      
+
+<div className=" mt-24">
+
+        <Footer/>
+</div>
+ 
+     
+      
+    </div>
+  )
+}
+
+export default Home
